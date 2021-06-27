da2a44946a9e24884b230364d3d4f7dc6877a952
Marc Klemm <marc.klemm@icloud.com>
2021-06-27 21:25:25 +0200

    Initial Commit

--- engine_euler.py
+++ engine_euler.py
@@ -1,13 +1,17 @@
 import numpy as np
 import json
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 '''Konstanten'''
 G = 6.67430e-11
 
 '''Einheiten'''
-ykg = 10e24 # yotta kg
-mkm = 10e9 # mega km
+# yotta kg
+ykg = 1e24
+# mega km
+mkm = 1e9
+# km
+km = 1e3
 
 '''Objekt-Array'''
 objekte = []
@@ -18,11 +22,6 @@ objekte = []
 def sqrt(x):
     # berechnet die Wurzel
     return x**0.5
-
-
-def sq(x):
-    # berechnet das Quadrat
-    return x**2
 
 
 '''Simulation'''
@@ -37,6 +36,10 @@ class Objekt:
         objekte.append(self)
 
 
+# fig = plt.figure(figsize=(4, 4))
+# ax = fig.add_subplot(111, projection='3d')
+
+
 def gravitation(obj1, obj2):
     richtungs_vek = np.array([0., 0., 0.])
 
@@ -45,17 +48,19 @@ def gravitation(obj1, obj2):
         richtungs_vek[i] = obj2.pos[i]-obj1.pos[i]
 
     '''Betrag des Vektors'''
-    betrag = sqrt(sq(richtungs_vek[0]) + sq(richtungs_vek[1]) + sq(richtungs_vek[2]))
+    betrag = sqrt(richtungs_vek[0]**2 + richtungs_vek[1]**2 + richtungs_vek[2]**2)
 
     '''Beschleunigungsvektor'''
     for i in range(0, 3):
-        obj1.a_vek[i] = - G * obj2.masse/betrag**3 * richtungs_vek[i]
+        obj1.a_vek[i] = G * obj2.masse/betrag**3 * richtungs_vek[i]
+
 
 def result(pos1, pos2):
     x = 0
     print(pos1, pos2)
     for i in range(0, 3):
-         x += sqrt(sq(pos2[i]-pos1[i]))
+        x += (pos2[i]-pos1[i])**2
+    x = sqrt(x)
     return x
 
 
@@ -67,29 +72,26 @@ def simulation(dt, t):
 
         '''berechnet die Position aller Objekte'''
         for obj1 in objekte:
-            ax.scatter(obj1.pos[0], obj1.pos[1], obj1.pos[2])
             for obj2 in objekte:
                 if obj1 != obj2:
                     gravitation(obj1, obj2)
-        print(vergangene_t, 'Erde: ', objekte[0].pos, 'Mond: ', objekte[1].pos)
+        print(f'{vergangene_t}, {objekte[0].pos=}, {objekte[1].pos=}')
 
         for obj in objekte:
 
             '''berechnet die Position nach dt'''
             for i in range(0, 3):
-                obj.pos[i] += obj.v_vek[i] * dt + 0.5 * obj.a_vek[i] * sq(dt)
+                obj.pos[i] += obj.v_vek[i] * dt + 0.5 * obj.a_vek[i] * dt**2
 
             '''berechnet den Geschwindigkeits Vektor'''
             for i in range(0, 3):
                 obj.v_vek[i] += obj.a_vek[i] * dt
+            # ax.scatter(obj.pos[0], obj.pos[1], obj.pos[2])
         vergangene_t += dt
     print(result(anfangs_vek, objekte[1].pos))
+    # plt.show()
 
 
 def output(data, output_file):
     with open(output_file, 'a') as log:
         json.dump(data, log, indent=len(data), separators=(',', ':'))
-
-
-fig = plt.figure(figsize=(4, 4))
-ax = fig.add_subplot(111, projection='3d')
--- main.py
+++ main.py
@@ -3,9 +3,20 @@ from engine_euler import *
 
 
 erde = Objekt(masse=5.9724 * ykg)
-mond = Objekt(masse=0.07346 * ykg, pos=np.array([0.3844 * mkm, 0., 0.]), v_vek=np.array([0., 1.022 * 1000, 0.]))
+mond = Objekt(masse=0.07346 * ykg, pos=np.array([0.3844 * mkm, 0., 0.]), v_vek=np.array([0., 1.022 * km, 0.]))
 
 
-simulation(60, 27.3217 * 24 * 3600)
+def main():
+    import cProfile
+    import pstats
 
-plt.show()
+    with cProfile.Profile() as pr:
+        simulation(60 * 10, 27.4 * 24 * 3600)
+
+    stats = pstats.Stats(pr)
+    stats.sort_stats(pstats.SortKey.TIME)
+    stats.print_stats()
+    stats.dump_stats(filename='main.prof')
+
+if __name__ == '__main__':
+    main()
