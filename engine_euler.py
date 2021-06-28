755d7d7a3c2f54f76334fce44a1f65a35dddcbfe
marcklemm <marc.klemm@icloud.com>
2021-06-28 09:04:52 +0200

    Initial Commit

--- engine_euler.py
+++ engine_euler.py
@@ -6,12 +6,28 @@ import matplotlib.pyplot as plt
 G = 6.67430e-11
 
 
+"""Funkationen"""
+
+
+def betrag(vek):
+    return np.sqrt(np.sum(vek**2))
+
+
 '''Simulation'''
 
 
-fig = plt.figure(figsize=(4, 4))
+fig = plt.figure(figsize=(10, 10))
 
 objekte = [] # beinhaltet die zu simulierenden Objekte
+
+
+def exzentrizitaet(obj):
+    return ''
+    apphelion = max([x for x in obj.coordinates])
+    perihelion = min([betrag(x) for x in obj.coordinates])
+    print(apphelion, perihelion)
+    exzent = 1-(2/(apphelion/perihelion)+1)
+    return exzent
 
 
 class Objekt:
@@ -20,33 +36,35 @@ class Objekt:
         self.pos = pos
         self.a_vek = a_vek
         self.v_vek = v_vek
+        self.coordinates = [[], [], []]
+        self.abstand = []
         self.ax = fig.add_subplot(111, projection='3d')
         objekte.append(self)
 
 
 def gravitation(obj1, obj2):
-    richtungs_vek = np.array([0., 0., 0.])
     richtungs_vek = obj2.pos-obj1.pos
-    betrag = np.sqrt(np.sum(richtungs_vek**2))
-    obj1.a_vek = G * obj2.masse/betrag**3 * richtungs_vek
+    abstand = betrag(richtungs_vek)
+    obj1.abstand.append(abstand)
+    obj1.a_vek = G * obj2.masse/abstand**3 * richtungs_vek
 
 
 def simulation(dt, t):
-    coordinates = [[], [], []]
     vergangene_t = 0
     while vergangene_t <= t:
         for obj in objekte:
             obj.pos += obj.v_vek * dt + 0.5 * obj.a_vek * dt**2
             obj.v_vek += obj.a_vek * dt
             for i in range(3):
-                coordinates[i].append(objekte[1].pos[i])
+                obj.coordinates[i].append(objekte[1].pos[i])
             for obj1 in objekte: # berechnet die Position aller Objekte
                 for obj2 in objekte:
                     if obj1 != obj2:
                         gravitation(obj1, obj2)
         vergangene_t += dt
     for obj in objekte:
-        obj.ax.scatter(coordinates[0], coordinates[1], coordinates[2])
+        obj.ax.scatter(obj.coordinates[0], obj.coordinates[1], obj.coordinates[2])
+        print(exzentrizitaet(obj))
     plt.show()
 
 
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from engine_euler import *
 
 
 sonne = Objekt(masse=1988500e24)
-erde = Objekt(masse=5.9724e24, pos=np.array([149.596e9, 0., 0.]), v_vek=np.array([0., 29.78e3, 0.]))
+erde = Objekt(masse=5.9724e24, pos=np.array([147.092e9, 0., 0.]), v_vek=np.array([0., 30.29e3, 0.]))
 # jupiter = Objekt(masse=1898.19e24, pos=np.array([778.570e9, 0., 0.]), v_vek=np.array([0., 13.06e3, 0.]))
 
 def main():
