6ce409c83f28b18bbbf22c841584b6f9c4f67a33
marcklemm <marc.klemm@icloud.com>
2021-06-29 18:18:52 +0200

    Initial Commit

--- engine_euler.py
+++ engine_euler.py
@@ -6,6 +6,10 @@ import matplotlib.pyplot as plt
 G = 6.67430e-11
 
 
+"""EInheiten"""
+yr = 365 * 24 * 3600
+
+
 """Funktionen"""
 def betrag(vek):
     return np.sqrt(np.sum(vek**2))
@@ -25,56 +29,54 @@ def output(data, output_file):
 
 '''Simulation'''
 fig = plt.figure(figsize=(10, 10)) # für matplot Simulation
+ax = fig.add_subplot(111)
 
 objekte = [] # beinhaltet die zu simulierenden Objekte
 
 
-
-
 class Objekt:
-    def __init__(self, masse=1, pos=np.array([0., 0., 0.]), a_vek=np.array([0., 0., 0.]), v_vek=np.array([0., 0., 0.])):
+    def __init__(self, masse=1, a=np.array([0., 0., 0.]), v=np.array([0., 0., 0.]), r=np.array([0., 0., 0.])):
         self.masse = masse
-        self.pos = pos
-        self.a_vek = a_vek
-        self.v_vek = v_vek
+        self.a = a
+        self.v = v
+        self.r = r
+        self.coordinates = []
         self.x = []
         self.y = []
         self.z = []
         self.abstand = []
-        self.ax = fig.add_subplot(111, projection='3d')
         objekte.append(self)
 
     def abstand_zu_stern(self):
-        self.abstand.append(betrag(self.pos - objekte[0].pos))
+        self.abstand.append(betrag(self.r - objekte[0].r))
 
-    def pos_update(self):
-        self.x.append(self.pos[0])
-        self.y.append(self.pos[1])
-        self.z.append(self.pos[2])
+    def r_update(self):
+        self.x += [r[0] for r in self.coordinates]
+        self.y += [r[1] for r in self.coordinates]
+        self.z += [r[2] for r in self.coordinates]
 
-
-def gravitation(obj1, obj2):
-    richtungs_vek = obj2.pos-obj1.pos
-    abstand = betrag(richtungs_vek)
-    obj1.a_vek = G * obj2.masse/abstand**3 * richtungs_vek
+    def gravitation(self):
+        for obj in objekte:
+            if obj != self:
+                richtungs_vek = obj.r - self.r
+                abstand = betrag(richtungs_vek)
+                self.a = G * obj.masse / abstand ** 3 * richtungs_vek
 
 
 def simulation(dt, t, name=""):
     vergangene_t = 0
     while vergangene_t <= t:
         for obj in objekte:
-            obj.pos += obj.v_vek * dt + 0.5 * obj.a_vek * dt**2 # berechnet die Position aller Objekte
-            obj.pos_update()
-            obj.v_vek += obj.a_vek * dt # berechnet die Geschwindigkeit der Objekte
             obj.abstand_zu_stern()
-            for obj1 in objekte: # berechnet die Beschleunigungen der Objekte
-                for obj2 in objekte:
-                    if obj1 != obj2:
-                        gravitation(obj1, obj2)
+            obj.gravitation() # berechnet die Beschleunigungen der Objekte
+            obj.r += obj.v * dt + 0.5 * obj.a * dt ** 2  # berechnet die Position aller Objekte
+            obj.coordinates.append([r for r in obj.r])
+            obj.v += obj.a * dt  # berechnet die Geschwindigkeit der Objekte
         vergangene_t += dt
     for obj in objekte:
-        obj.ax.scatter(obj.x, obj.y, obj.z)
-    output({"Name": name, "dt": dt, "t": t, "exzentrizitaet Erde": exzentrizitaet(objekte[1])}, 'log.json')
+        obj.r_update()
+        ax.scatter(obj.x, obj.y)
+    #output({"Name": name, "dt": dt, "t": t, "exzentrizitaet Mars": exzentrizitaet(objekte[1])}, 'log.json')
     plt.show()
 
 
--- main.py
+++ main.py
@@ -3,15 +3,15 @@ from engine_euler import *
 
 
 sonne = Objekt(masse=1988500e24)
-erde = Objekt(masse=5.9724e24, pos=np.array([147.092e9, 0., 0.]), v_vek=np.array([0., 30.29e3, 0.]))
-# jupiter = Objekt(masse=1898.19e24, pos=np.array([778.570e9, 0., 0.]), v_vek=np.array([0., 13.06e3, 0.]))
+#erde = Objekt(masse=5.9724e24, r=np.array([147.092e9, 0., 0.]), v=np.array([0., 30.29e3, 0.]))
+mars = Objekt(masse=0.64171e24, r=np.array([-206.617e9, 0., 0.]), v=np.array([0., -26.50e3, 0.]))
 
 def main():
     import cProfile
     import pstats
 
     with cProfile.Profile() as pr:
-        simulation(10 * 60, 1 * 365 * 24 * 3600, "Erde, Sonne und Jupiter")
+        simulation(10 * 60, 1 * yr, "Sonne und Mars")
 
     stats = pstats.Stats(pr)
     stats.sort_stats(pstats.SortKey.TIME)
